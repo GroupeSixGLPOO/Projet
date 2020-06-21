a0abded475b7be0968ca5030c76069a1ae97f4c5
Houyu0926 <64799132+Houyu0926@users.noreply.github.com>
2020-06-21 20:16:39 +0200

    [List contacts et database]

--- src/main/java/client/ClientThread.java
+++ src/main/java/client/ClientThread.java
@@ -1,42 +1,24 @@
 package client;
 
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.net.Socket;
-/**
- *   作用：一直接收服务端转发过来的信息
- * */
+
+
 public class ClientThread extends Thread {
 
-    private Socket socket;
+    private Socket clientSocket;
     public ClientThread(Socket socket) {
-        this.socket = socket;
+        this.clientSocket = socket;
     }
 
     public void run() {
         try {
-            InputStream inputStream = socket.getInputStream();
-            InputStreamReader inputStreamReader = new InputStreamReader(
-                    inputStream);
-            BufferedReader br = new BufferedReader(inputStreamReader);
-            try {
-                // 信息的格式：(login||logout||say),发送人,收发人,信息体
-                while (true) {
-//                    String msg=br.readLine();
-//                    System.out.println(msg);
-//                    String[] str = msg.split(",");
-//                    switch (str[0]) {
-//                        case "say":
-//                            System.out.println(str[2] + " 对   " + str[1] + " say:  "
-//                                    + str[3]);
-//                            break;
-//                        default:
-//                            break;
-//                    }
-                }
+            OutputStream os = clientSocket.getOutputStream();
+            InputStream is = clientSocket.getInputStream();
+
+
+
             } catch (Exception e) {
                 e.printStackTrace();
             }
